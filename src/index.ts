642ecb9653a7dfb81d3d82d3a9bafa7beb4a3727
Vladislav Suchkov <20380250+murolem@users.noreply.github.com>
2024-03-01 23:08:06 +1000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 $(() => {
     var tabButtons = $('.portable-infobox .pi-panel-scroll-wrapper .pi-section-navigation li');
     tabButtons.on('mouseenter', e => {
-        console.log(e);
         e.currentTarget.click()
     });
 });
